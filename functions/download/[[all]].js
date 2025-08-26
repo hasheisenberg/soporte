ff5422b9237a13194a8c1b96e29a04115f76a6be
manuel <106616149+hasheisenberg@users.noreply.github.com>
2025-08-26 15:32:08 -0400

    234

--- functions/download/[[all]].js
+++ functions/download/[[all]].js
@@ -2,12 +2,14 @@ export async function onRequestGet(context) {
   const url = new URL(context.request.url);
   const key = url.pathname.slice(1); // Elimina el primer "/"
   const obj = await context.env.BUCKET.get(key);
+  console.log(url, key, obj);
 
   if (!obj) {
     return new Response("Archivo no encontrado", { status: 404 });
   }
 
   const headers = new Headers();
+
   obj.writeHttpMetadata(headers);
   headers.set("etag", obj.httpEtag);
 
